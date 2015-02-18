0296026d85d16205c10c18a59db4e11d2cf45a4e
RaD1KaL <sieger_116@mail.ru>
2015-02-18 15:49:37 +0200

    improve logging

--- test/src/org/omegat/util/network/ProxyUtilsTest.java
+++ test/src/org/omegat/util/network/ProxyUtilsTest.java
@@ -5,35 +5,38 @@ import org.junit.Test;
 import java.io.IOException;
 import java.net.*;
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
- * Created by rad1kal on 29.10.2014.
+ * Created by stsypanov on 29.10.2014.
  */
 public class ProxyUtilsTest {
-    protected String googleURL = "https://www.google.com";
+	private static final Logger logger = Logger.getLogger(ProxyUtilsTest.class.getName());
+	private String url = "https://www.mail.ru";
 
-    @Test
-    public void testConnection() throws Exception{
-        URL google = new URL(googleURL);
-        List<Proxy> proxies = ProxyUtils.getProxySelector(googleURL);
 
-        for (Proxy proxy : proxies){
-            int responseCode = getResponseCode(google, proxy);
+	@Test
+	public void testConnection() throws Exception {
+		URL google = new URL(url);
+		List<Proxy> proxies = ProxyUtils.getProxySelector(url);
 
-            System.out.println(proxy.toString() + " " + responseCode);
-        }
+		for (Proxy proxy : proxies) {
+			int responseCode = getResponseCode(google, proxy);
 
-    }
+			logger.info(proxy.toString() + ' ' + responseCode);
+		}
 
-    @Test
-    public void testProxySelector() throws Exception {
-        Proxy proxy = ProxyUtils.getProxy(googleURL);
+	}
 
-        System.out.println(proxy.toString() + " " + getResponseCode(new URL(googleURL), proxy));
-    }
+	@Test
+	public void testProxySelector() throws Exception {
+		Proxy proxy = ProxyUtils.getProxy(url);
 
-    private int getResponseCode(URL google, Proxy proxy) throws IOException {
-        HttpURLConnection urlConnection = (HttpURLConnection) google.openConnection(proxy);
-        return urlConnection.getResponseCode();
-    }
+		logger.info(proxy.toString() + ' ' + getResponseCode(new URL(url), proxy));
+	}
+
+	private static int getResponseCode(URL google, Proxy proxy) throws IOException {
+		HttpURLConnection urlConnection = (HttpURLConnection) google.openConnection(proxy);
+		return urlConnection.getResponseCode();
+	}
 }
