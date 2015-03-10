d9e9e0f597b471dd0099ef9f315147e2f634800a
RaD1KaL <sieger_116@mail.ru>
2015-03-10 21:45:26 +0200

    merge origin into java7

--- src/org/omegat/gui/filelist/ProjectFilesListController.java
+++ src/org/omegat/gui/filelist/ProjectFilesListController.java
@@ -30,14 +30,7 @@
 
 package org.omegat.gui.filelist;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.Component;
-import java.awt.Cursor;
-import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
