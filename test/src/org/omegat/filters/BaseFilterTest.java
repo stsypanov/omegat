cfd0ef48c32bb532962a373c74ce7d7697b84d6f
stsypanov <sergei.tsypanov@yandex.ru>
2015-05-06 19:01:08 +0300

    add filter for emails and urls

--- test/src/org/omegat/filters/BaseFilterTest.java
+++ test/src/org/omegat/filters/BaseFilterTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertFalse;
 
 /**
  * This rest checks if BaseFilter passes through the segments with dates and numbers
- *
+ * <p/>
  * Created by stsypanov on 05.05.2015.
  */
 public class BaseFilterTest {
@@ -30,6 +30,11 @@ public class BaseFilterTest {
 			"0",
 			"0.0"};
 
+	public static final String[] emails = {
+			"sieger_116@mail.ru",
+			"sergei.tsypanov@yandex.ru"
+	};
+
 	@Test
 	public void testDates() throws Exception {
 		for (String date : dates) {
@@ -41,8 +46,17 @@ public class BaseFilterTest {
 
 	@Test
 	public void testNumbers() throws Exception {
-		for (String number : numbers){
+		for (String number : numbers) {
 			EntryKey entryKey = new EntryKey(number);
+			SourceTextEntry entry = new SourceTextEntry(entryKey);
+			assertFalse(filter.allowed(entry));
+		}
+	}
+
+	@Test
+	public void testEmails() throws Exception {
+		for (String email : emails){
+			EntryKey entryKey = new EntryKey(email);
 			SourceTextEntry entry = new SourceTextEntry(entryKey);
 			assertFalse(filter.allowed(entry));
 		}
