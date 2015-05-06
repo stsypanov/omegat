621c6d3d8c6db1a0cfda51728edda5327fd67768
stsypanov <sergei.tsypanov@yandex.ru>
2015-05-06 19:01:08 +0300

    add filter for emails and urls
    (cherry picked from commit cfd0ef48c32bb532962a373c74ce7d7697b84d6f)

--- src/org/omegat/gui/editor/filter/BaseFilter.java
+++ src/org/omegat/gui/editor/filter/BaseFilter.java
@@ -10,23 +10,31 @@ import java.util.regex.Pattern;
 
 /**
  * Provides base source text filtering for project.
- * It helps to get rid of showing segments with numbers, dates, etc
+ * It helps to get rid of showing segments with numbers, dates, emails, URI's etc
  * which are usually remain the same for all languages.
- *
+ * <p/>
  * Created by stsypanov on 22.04.2015.
  */
 public class BaseFilter implements IEditorFilter {
 
+	private static final JPanel controlComponentStub = new JPanel();
+
 	private static final Pattern NUMBER_PATTERN = Pattern.compile("^(?:(\\d+(?:[\\.,]\\d+)?)(?:\\|)?)+");
 	private static final Pattern DATE_PATTERN = Pattern.compile("(0[1-9]|[12][0-9]|3[01])[- /.]([1-9]|[1-9]|0[1-9]|1[012])[- /.](19|20)\\d\\d");
-//	private static final Pattern DATE_PATTERN = Pattern.compile("[A-Za-z\\s]+");
-	private static final JPanel controlComponentStub = new JPanel();
+	private static final Pattern EMAIL_PATTERN = Pattern.compile("^[-\\w.]+@([A-z0-9][-A-z0-9]+\\.)+[A-z]{2,4}$");
+	private static final Pattern URL_PATTERN = Pattern.compile("((([A-Za-z]{3,9}:(?:\\/\\/)?)(?:[\\-;:&=\\+\\$,\\w]+@)?[A-Za-z0-9\\.\\-]+|(?:www\\.|[\\-;:&=\\+\\$,\\w]+@)[A-Za-z0-9\\.\\-]+)((?:\\/[\\+~%\\/\\.\\w\\-_]*)?\\??(?:[\\-\\+=&;%@\\.\\w_]*)#?(?:[\\.\\!\\/\\\\\\w]*))?)");
+
 
 	@Override
 	public boolean allowed(SourceTextEntry ste) {
 		EntryKey key = ste.getKey();
 		String sourceText = key.sourceText;
-		return !(NUMBER_PATTERN.matcher(sourceText).matches() || DATE_PATTERN.matcher(sourceText).matches());
+		return !(
+				NUMBER_PATTERN.matcher(sourceText).matches() ||
+						DATE_PATTERN.matcher(sourceText).matches() ||
+						EMAIL_PATTERN.matcher(sourceText).matches() ||
+						URL_PATTERN.matcher(sourceText).matches()
+		);
 	}
 
 	@Override
