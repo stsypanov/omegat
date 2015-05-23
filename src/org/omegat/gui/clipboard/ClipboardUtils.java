3a45b03ac762bb0d9d995685e410b6e1cb581b53
stsypanov <STsypanov@luxoft.com>
2015-05-23 22:46:56 +0300

    introduce Clipboard controller and improve clipboard utils: now all copied items are added into clipboard (cherry picked from commit 13d22d4)

--- src/org/omegat/gui/clipboard/ClipboardUtils.java
+++ src/org/omegat/gui/clipboard/ClipboardUtils.java
@@ -2,11 +2,17 @@ package org.omegat.gui.clipboard;
 
 import javax.swing.*;
 import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultEditorKit;
 import javax.swing.text.Document;
 import javax.swing.text.JTextComponent;
 import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.Transferable;
+import java.awt.datatransfer.UnsupportedFlavorException;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.io.IOException;
 
 /**
  * Created with IntelliJ IDEA.
@@ -16,41 +22,69 @@ import java.awt.event.KeyEvent;
  */
 public class ClipboardUtils {
 
-	public static void bind(JTextComponent component, Frame owner) {
-		InputMap im = component.getInputMap(JComponent.WHEN_FOCUSED);
-		ActionMap am = component.getActionMap();
-		String key = "insert from clipboard";
-		KeyStroke keyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_V, KeyEvent.CTRL_MASK + KeyEvent.SHIFT_DOWN_MASK, true);
-		im.put(keyStroke, key);
-		am.put(key, getAction(component, owner));
-	}
+    private static ClipboardController controller;
 
-	private static Action getAction(final JTextComponent component, final Frame owner) {
-		return new AbstractAction() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				showDialogAndPaste(component, owner);
-			}
-		};
-	}
+    //todo don't create controller every time
+    public static void bind(JTextComponent component, Frame owner) {
+        controller = new ClipboardController(ClipboardDialog.getInstance(owner));
+        InputMap im = component.getInputMap(JComponent.WHEN_FOCUSED);
+        ActionMap am = component.getActionMap();
+        String key = "insert from clipboard";
+        KeyStroke keyStroke = KeyStroke.getKeyStroke(KeyEvent.VK_V, KeyEvent.CTRL_MASK + KeyEvent.SHIFT_DOWN_MASK, true);
+        im.put(keyStroke, key);
+        am.put(key, getAction(component, owner));
 
-	private static void showDialogAndPaste(JTextComponent component, Frame owner) {
-		ClipboardDialog dialog = ClipboardDialog.getInstance(owner);
-		dialog.showDialog();
-		String selected = dialog.getSelected();
-		if (component instanceof JTextArea) {
-			((JTextArea) component).insert(selected, component.getCaretPosition());
-			dialog.hideDialog();
-		}
-		if (component instanceof JEditorPane) {
-			Document document = component.getDocument();
-			try {
-				document.insertString(component.getCaretPosition(), selected, null);
-				dialog.hideDialog();
-			} catch (BadLocationException e) {
-				Log.log(e);
-			}
-		}
-		dialog.setSelected(null);
-	}
+        bindCopyIntoClipboardItemsAction(im, am);
+
+    }
+
+    private static void bindCopyIntoClipboardItemsAction(InputMap im, ActionMap am) {
+        String key1 = "copy-to-dialog-clipboard";
+        KeyStroke keyStroke1 = KeyStroke.getKeyStroke(KeyEvent.VK_C, KeyEvent.CTRL_MASK, true);
+        im.put(keyStroke1, key1);
+        am.put(key1, new DefaultEditorKit.CopyAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Clipboard systemClipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+                Transferable contents = systemClipboard.getContents(null);
+
+                if (contents != null && contents.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+                    try {
+                        String s = (String) contents.getTransferData(DataFlavor.stringFlavor);
+                        controller.addString(s);
+                    } catch (UnsupportedFlavorException | IOException ex) {
+                        Log.severe("cannot paste into clipboard", ex);
+                    }
+                }
+            }
+        });
+    }
+
+    private static Action getAction(final JTextComponent component, final Frame owner) {
+        return new AbstractAction() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showDialogAndPaste(component, owner);
+            }
+        };
+    }
+
+    private static void showDialogAndPaste(JTextComponent component, Frame owner) {
+        controller.showDialog();
+        String selected = controller.getSelected();
+        if (component instanceof JTextArea) {
+            ((JTextArea) component).insert(selected, component.getCaretPosition());
+            controller.hideDialog();
+        }
+        if (component instanceof JEditorPane) {
+            Document document = component.getDocument();
+            try {
+                document.insertString(component.getCaretPosition(), selected, null);
+                controller.hideDialog();
+            } catch (BadLocationException e) {
+                Log.log(e);
+            }
+        }
+        controller.setSelected(null);
+    }
 }
