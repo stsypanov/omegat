dc483f7ca60d3fdfbeb3dbc31e598f789eb9ea5b
Sergei <STsypanov@luxoft.com>
2016-04-18 13:08:46 +0300

    working prototype (cherry picked from commit 79aa268)

---
+++ src/org/omegat/gui/dictionaries/DictionaryPopupModel.java
@@ -0,0 +1,37 @@
+package org.omegat.gui.dictionaries;
+
+import org.omegat.core.dictionaries.BaseDictionariesManager;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+public class DictionaryPopupModel implements SearchByKeyProvider {
+
+    private List<String> keys;
+    private BaseDictionariesManager dictionariesManager;
+
+    public DictionaryPopupModel(BaseDictionariesManager dictionariesManager) {
+        this.dictionariesManager = dictionariesManager;
+        keys = new ArrayList<>();
+    }
+
+    public List<String> getKeys() {
+        return keys;
+    }
+
+    public void clear() {
+        keys.clear();
+    }
+
+    @Override
+    public List<String> findByKey(String key) {
+       if (key.length() >= 2){
+           Set<String> keys = dictionariesManager.getKeys(key);
+           return new ArrayList<>(keys);
+       } else {
+           return Collections.emptyList();
+       }
+    }
+}
